109ef287db6c4eae76c2a239089825d8e1334956
Ariel Walley <arielwalley95@gmail.com>
2020-05-02 21:14:09 -0400

    Updating three doors
    Restructing CSS
    Allowing JS functions for all doors
    Testing beachDoorPath and spaceDoorPath variables

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
 
 let botDoorPath = 'https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/robot.svg';
+let beachDoorPath = 'https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/beach.svg';
+let spaceDoorPath = 'https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/space.svg';
 
 let door1 = document.getElementById('door1');
+let door2 = document.getElementById('door2');
+let door3 = document.getElementById('door3');
 
 door1.onclick = () => {
     door1.src = botDoorPath;
 };
+
+door2.onclick = () => {
+    door2.src = beachDoorPath;
+};
+
+door3.onclick = () => {
+    door3.src = spaceDoorPath;
+};
+
 
 
