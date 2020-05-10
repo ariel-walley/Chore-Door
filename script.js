5802ff43aff97a4135f5dab5eed614d2ae203577
Ariel Walley <arielwalley95@gmail.com>
2020-05-10 11:30:28 -0400

    Win and lose func, bestStreak
    Restructed JS slightly to add a win function and lose function
    
    Also added funcationality for the Best Streak box

--- script.js
+++ script.js
@@ -3,30 +3,46 @@ let beachDoor = 'https://s3.amazonaws.com/codecademy-content/projects/chore-door
 let spaceDoor = 'https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/space.svg';
 let defaultDoor = 'https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/closed_door.svg';
 
-
+let door1 = document.getElementById('door1');
+let door2 = document.getElementById('door2');
+let door3 = document.getElementById('door3');
 
 let turnCounter = 0;
-let gameCounter = 0;
+let currentStreak = 0;
+let bestStreak = 0;
+
+function win () {
+    turnCounter = 0;
+    currentStreak++;
+    document.getElementById('currentStreakBox').innerHTML = currentStreak;
+        
+    if (currentStreak > bestStreak) {
+        bestStreak = currentStreak;
+        document.getElementById('bestStreakBox').innerHTML = bestStreak;
+    }
+};
+
+function lose () {
+    turnCounter = 0;
+    currentStreak = 0;
+    document.getElementById('currentStreakBox').innerHTML = currentStreak;
+}
 
 function doorPaths (param) {
-
-    let door1 = document.getElementById('door1');
-    let door2 = document.getElementById('door2');
-    let door3 = document.getElementById('door3');
-    let currentStreakBox = document.getElementById('currentStreakBox');
 
     let randomFirstClick = Math.floor(Math.random() * 3); //prints 0, 1, or 2
     let randomSecondClick = Math.floor(Math.random() *2); //prints 0 or 1
 
     if (door1.src === botDoor || door2.src === botDoor || door3.src === botDoor) { //Checking if any robots to end game
         console.log('Oops, there\'s a robot. Game over!');
+        lose();
         return;
     } else if (param.src !== defaultDoor) { //Checking for clicking the same door twice
         console.log('Silly goose! You can\'t click on the same door twice!')
         return;
     } else if (turnCounter === 0) {  //For deciding what's behind door number one!
         if (randomFirstClick === 0) {
-            turnCounter++;
+            lose();
             return param.src = botDoor;
         } else if (randomFirstClick === 1) {
             turnCounter++;
@@ -41,7 +57,7 @@ function doorPaths (param) {
                 turnCounter++;
                 return param.src = beachDoor;
             } else {
-                turnCounter++;
+                lose();
                 return param.src = botDoor;
             }
         } else if (door1.src === beachDoor || door2.src === beachDoor || door3.src === beachDoor) {
@@ -49,17 +65,22 @@ function doorPaths (param) {
                 turnCounter++;
                 return param.src = spaceDoor;
             } else {
-                turnCounter++;
+                lose();
                 return param.src = botDoor;
             }
         }        
     } else { // For deciding what's behind door number three - end of game
         param.src = botDoor;
-        turnCounter++;
-        gameCounter++;
-        currentStreakBox.innerHTML = gameCounter;
+        win();
         return;
-    };
+    };     
+};
+
+function reset () {
+    door1.src = defaultDoor;
+    door2.src = defaultDoor;
+    door3.src = defaultDoor;
+    return;
 };
 
 /* brainstorming:
